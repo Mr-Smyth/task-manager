10834334767b8c611877a34ffb8c2ae7dbf68268
eamonn <e.smyth75@gmail.com>
2024-07-22 12:09:21 +0100

    Added Ember relationships to user and tasks models. Also created a custom adaptor to tell ember where to get the data from specifically

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,19 @@
-import Model, { attr } from '@ember-data/model';
+// old code
+// import Model, { attr } from '@ember-data/model';
+
+// export default class UserModel extends Model {
+//   @attr('string') name;
+//   @attr('string') description;
+//   @attr() taskIds; // an array of task ids
+// }
+
+// new code
+import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class UserModel extends Model {
   @attr('string') name;
   @attr('string') description;
-  @attr() taskIds; // an array of task ids
+  // Tell Ember Data to load the related records asynchronously
+  // Also tell Ember Data that the tasks relationship is the inverse of the user relationship in the TaskModel.
+  @hasMany('task', { async: true, inverse: 'user' }) tasks;
 }
--- app/routes/users.js
+++ app/routes/users.js
@@ -9,7 +9,7 @@ export default class UsersRoute extends Route {
     // Load users and tasks concurrently
     await Promise.all([
       this.dataLoader.loadUsers(),
-      this.dataLoader.loadTasks()
+      this.dataLoader.loadTasks(),
     ]);
 
     // Retrieve all users and tasks from the store
@@ -17,13 +17,13 @@ export default class UsersRoute extends Route {
     let allTasks = this.store.peekAll('task');
 
     // Filter out users whose name is 'Unassigned' because i dont want them in the list of users
-    let filteredUsers = allUsers.filter(user => user.name !== 'Unassigned');
+    let filteredUsers = allUsers.filter((user) => user.name !== 'Unassigned');
 
     // Associate tasks with each user based on taskIds currently linked to a user
-    filteredUsers.forEach(user => {
+    filteredUsers.forEach((user) => {
       let userTasks = [];
-      user.taskIds.forEach(taskId => {
-        let task = allTasks.find(task => task.id === taskId);
+      user.taskIds.forEach((taskId) => {
+        let task = allTasks.find((task) => task.id === taskId);
         if (task) {
           userTasks.push(task);
         }
