e30bebe64b3c9f526096538a5e21da6f2ea7d93a
eamonn <e.smyth75@gmail.com>
2024-08-01 13:34:49 +0100

    Moved seperation of tasks to the tasks controller - which also fixed the need for refresh()

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -2,28 +2,58 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class TasksController extends Controller {
-  // Inject router and store services
-  @service router;
   @service store;
+  // track the tasks from the model provided in the route
+  @tracked tasks = this.model.tasks;
+  @tracked users = this.model.users;
 
-  // Define the assignUser action to assign a user to a task
+  get unassignedTasks() {
+    // Filtering tasks in an filter/if where the user name is 'Unassigned' - moved from route
+    return this.tasks.filter((task) => {
+      let userName = task.get('user') ? task.get('user').get('name') : 'Unassigned';
+      return userName === 'Unassigned';
+    });
+  }
+
+  get assignedTasks() {
+    // Filtering tasks in an filter/if where the user name is not 'Unassigned' - moved from route
+    return this.tasks.filter((task) => {
+      let userName = task.get('user') ? task.get('user').get('name') : 'Unassigned';
+      return userName !== 'Unassigned';
+    });
+  }
+
   @action
-  async assignUser(taskId, userId) {
-    console.log('Inside assignUser: app/controllers/tasks.js');
-    console.log('Task ID:', taskId, 'User ID:', userId);
+  assignUser(taskId, userId) {
 
-    // Retrieve the task and user records by their IDs
-    let task = this.store.peekRecord('task', taskId);
-    let user = this.store.peekRecord('user', userId);
+    // I found no possible way to remove the refresh option and still get it to work, so i began to redo my tasks.js route and integrate it more neatly into this controller
+    // It now seems to work, perhaps given my different approach above where i track the tasks and users, and get the different tasks
 
-    // If both task and user exist, assign the user to the task use set
-    // 
+
+    // Retrieve the task and user records by their IDs - This works - but should we always get from the store?
+    // let task = this.store.peekRecord('task', taskId);
+    // let user = this.store.peekRecord('user', userId);
+
+    // this works also getting the data from the model - which is the best approach?
+    // is it best to if {} this below code and have the above code in the else where we get from the store??
+    // let task = this.tasks.find((task) => task.id === taskId);
+    // let user = this.users.find((user) => user.id === userId);
+
+    // compromise - but which method is best - is this overkill?
+    // get tasks if in model, else get them from the store
+    let task =
+      this.tasks.find((task) => task.id === taskId) || this.store.peekRecord('task', taskId);
+    let user =
+      this.users.find((user) => user.id === userId) || this.store.peekRecord('user', userId);
+
+    // If both task and user exist, assign the user to the task
     if (task && user) {
       task.set('user', user);
-      this.router.refresh('tasks'); 
+    } else {
+      console.error('There is either no task or no user');
     }
   }
 }
-
--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -1,6 +1,5 @@
 // app/routes/tasks.js
 import Route from '@ember/routing/route';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class TasksRoute extends Route {
@@ -14,48 +13,13 @@ export default class TasksRoute extends Route {
       this.dataLoader.loadTasks(),
     ]);
 
+    // Return all tasks and users as is
     let tasks = this.store.peekAll('task');
     let users = this.store.peekAll('user');
 
-    // Filter tasks based on user relationship - looking for Unassigned tasks
-    let unassignedTasks = tasks.filter((task) => {
-      // get the user object
-      let user = task.get('user');
-      // Check if the user exists and get the name of the user
-      if (user) {
-        let userName = user.get('name');
-        if (userName === 'Unassigned') {
-          // If name is 'Unassigned', return true to include this task in Unassigned
-          return true;
-        }
-      }
-      // If any condition fails, return false to skip this task
-      return false;
-    });
-
-    // Filter tasks based on user relationship - looking for Assigned tasks
-    let assignedTasks = tasks.filter((task) => {
-      // get the user object
-      let user = task.get('user');
-      // Check if the user exists and get the name of the user
-      if (user) {
-        let userName = user.get('name');
-        if (userName !== 'Unassigned') {
-          // If name is  not 'Unassigned', return true to include this task in Assigned
-          return true;
-        }
-      }
-      // If any condition fails, return false to skip this task
-      return false;
-    });
-
-    // return the users and assigned/unassigned tasks as i need to pass the users to the user-select component
     return {
-      tasks: {
-        unassigned: unassignedTasks,
-        assigned: assignedTasks,
-      },
-      users,
+      tasks,
+      users
     };
   }
 }
