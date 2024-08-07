e3a6d7281314f106e04e1e2b840546a7a513369e
eamonn <e.smyth75@gmail.com>
2024-08-07 19:48:14 +0100

    Added the dataLoader to the users route to trigger the data service to load all users and tasks

--- app/routes/users.js
+++ app/routes/users.js
@@ -4,8 +4,15 @@ import { inject as service } from '@ember/service';
 
 export default class UsersRoute extends Route {
   @service store;
+  @service dataLoader;
+
 
   async model() {
+    // Load the tasks and users concurrently
+    await Promise.all([
+      this.dataLoader.loadUsers(),
+      this.dataLoader.loadTasks(),
+    ]);
     // Retrieve all users from the store
     let allUsers = this.store.peekAll('user');
 
