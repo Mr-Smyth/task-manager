3545fd4184b231505b9d25688920cb699d424ae5
eamonn <e.smyth75@gmail.com>
2024-08-07 17:21:03 +0100

    Add createUser route and template and add and style create user button

--- app/routes/users.js
+++ app/routes/users.js
@@ -6,13 +6,9 @@ export default class UsersRoute extends Route {
   @service store;
 
   async model() {
-    // Retrieve all users and tasks from the store
+    // Retrieve all users from the store
     let allUsers = this.store.peekAll('user');
 
-    // Filter out users whose name is 'Unassigned' because i dont want them in the list of users
-    // perhaps i should remove unassigned from the list of imported users and set it up as a static user??
-    let filteredUsers = allUsers.filter((user) => user.name !== 'Unassigned');
-
-    return filteredUsers;
+    return allUsers;
   }
 }
