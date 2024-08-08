3d36689a60e8b40c26dabc88db36132cc019ba77
eamonn <e.smyth75@gmail.com>
2024-08-08 11:25:16 +0100

    Complete the create user action in the users controller

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,8 +1,11 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class UsersController extends Controller {
+  @service store;
+
   // track the modal state in users template
   @tracked isModalOpen = false;
 
@@ -11,17 +14,33 @@ export default class UsersController extends Controller {
     this.isModalOpen = !this.isModalOpen;
   }
 
+  /* Simple method to get next available id */
+  get newId() {
+    // convert each id into a integer by getting the model which is the user model
+    let userIds = this.model.map((user) => parseInt(user.id, 10));
+    // Start at 1001 if no users exist
+    let maxId = Math.max(...userIds, 1001);
+    // return a new id
+    return (maxId + 1).toString();
+  }
+
   @action
   createUser(event) {
     event.preventDefault();
-    
-    // get the form values from the event target
-    const userName = event.target.userName.value;
-    const userDescription = event.target.userDescription.value;
 
-    // log data for now
-    console.log("Creating a user with name =", userName);
-    console.log("Creating a user with description =", userDescription);
+    // get the form values from the event target
+    let newId = this.newId;
+    let userName = event.target.userName.value;
+    let userDescription = event.target.userDescription.value;
+    let userTaskIds = [];
+
+    // Create a new user record and save it using createRecord
+    this.store.createRecord('user', {
+      id: newId,
+      name: userName,
+      description: userDescription,
+      taskIds: userTaskIds,
+    });
 
     this.toggleModal();
   }
